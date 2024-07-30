d3cfddd7edb6335840fbabe8ed801c6b1283d960
Saksham Srivastava <96429260+Saksham2716@users.noreply.github.com>
2024-07-30 15:10:34 +0530

    Update start_container.sh

--- python-app/start_container.sh
+++ python-app/start_container.sh
@@ -3,6 +3,7 @@ set -e
 
 # Pull the Docker image from Docker Hub
 echo
-
+docker pull $DOCKER_REGISTRY_URL/$DOCKER_REGISTRY_USERNAME/simple-python-flask-app:latest
 # Run the Docker image as a container
 echo
+docker run -d --name $DOCKER_REGISTRY_URL/$DOCKER_REGISTRY_USERNAME/simple-python-flask-app:latest
